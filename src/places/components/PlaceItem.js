83e886d6aa76061d91836e35d63075062ae274a7
Abduselam Ali <abduselamali12@gmail.com>
2023-09-19 23:54:14 +0300

    second commit

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,11 +1,14 @@
-import React, {useState} from "react";
+import React, {useState,useContext} from "react";
 import Card from '../../shared/components/UIElements/Card';
 import Button from "../../shared/components/FormElements/Button";
 import Modal from "../../shared/components/UIElements/Modal";
 import Map from '../../shared/components/UIElements/Map';
+import { AuthContext } from "../../shared/context/auth-context";
 import './PlaceItem.css';
 
 const PlaceItem = props =>{
+
+    const auth = useContext(AuthContext)
 
     const [showMap, setShowMap] = useState(false);
 
@@ -67,8 +70,8 @@ const PlaceItem = props =>{
 
         <div className="place-item__actions">
             <Button inverse onClick={openMapHandler}>View on Map</Button>
-            <Button to ={`/places/${props.id}`}>Edit</Button>
-            <Button danger onClick={showDeleteWarningHandler}>Delete</Button>
+            {auth.isLoggedIn && <Button  to ={`/places/${props.id}`}> Edit </Button> }
+            {auth.isLoggedIn && <Button danger onClick={showDeleteWarningHandler}>Delete</Button>}
         </div>
         </Card>
     </li>
--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -1,16 +1,100 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
 
 import Card from '../../shared/components/UIElements/Card';
+import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button';
+import {VALIDATOR_EMAIL,
+         VALIDATOR_MINLENGTH,
+      VALIDATOR_REQUIRE} from '../../shared/util/validators'
 
-import './Auth.css'
+import {useForm} from '../../shared/hooks/form-hook'
+import { AuthContext } from '../../shared/context/auth-context';
+         import './Auth.css'
 
 const Auth = () => {
- return <Card>
+   const auth = useContext(AuthContext);
+
+  const [isLoginMode, setIsLoginMode] = useState(true);
+
+  const [formState, inputHandler, setFormData] = useForm({
+      email:{
+         value:'',
+         isValid: false
+      },
+      password:{
+         value:'',
+         isValid:false
+      }
+
+  },
+  false
+  );
+
+   const switchModeHandler = () => {
+      if(!isLoginMode){
+            setFormData(
+               {
+                  ...formState.inputs,
+                  name: undefined
+               }, 
+               formState.inputs.email.isValid && formState.inputs.password.isValid 
+               );
+      } else {
+         setFormData({
+            ...formState.inputs,
+            name:{
+               value:'',
+               isValid:false
+            }
+         }, false
+         );
+      }
+      setIsLoginMode(prevMode => !prevMode)
+   };
+  const authSubmitHandler =  Event => {
+   Event.preventDefault();
+   console.log(formState.inputs);
+   auth.login();
+  };
+
+ return <Card className="authentication">
     <h2>Login Required</h2>
     <hr />
-    <form>
-     
+    <form onSubmit={authSubmitHandler}>
+      {!isLoginMode && <Input 
+         element="input"
+         id="name"
+         type="text"
+         label="User Name"
+         validators={[VALIDATOR_REQUIRE()]}
+         errorText="Please enter your user name" 
+         onInput={inputHandler}
+      />}
+      <Input
+         element="input"
+         id="email"
+         type="email"
+         label="E-Mail"
+         validators={[VALIDATOR_EMAIL()]}
+         errorText="Please enter a valid email address." 
+         onInput={inputHandler}
+         />
+      <Input
+         element="input"
+         id="password"
+         type="password"
+         label="Password"
+         validators={[VALIDATOR_MINLENGTH(5)]}
+         errorText="Please enter your password (atleast 5 character)." 
+         onInput={inputHandler}
+         />
+         <Button type="submit" disable ={!formState.isValid}>
+         {isLoginMode ?  'Login' : 'Signup' }
+         </Button>
     </form>
+    <Button inverse onClick={switchModeHandler}>
+    Switch To  {isLoginMode ? 'Signup' : 'Login'}
+    </Button>
  </Card>
 } 
 
